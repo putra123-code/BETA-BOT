145d1ff29ebba112108df2879aa848ab0ef8c2ab
putra123-code <73972426+putra123-code@users.noreply.github.com>
2020-11-20 14:33:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -772,7 +772,7 @@ if (text.includes("$loli"))
 if (text.includes("$lirik")){
 	const teks = text.split("$lirik")[1]
 	axios.get(`http://scrap.terhambar.com/lirik?word=${teks}`).then ((res) => {
-	 	let hasil = `*LIRIK LAGU* ${ *{teks}* }\n\n\n ${res.data.result.lirik}`
+	 	let hasil = `*LIRIK LAGU* ${{teks}}\n\n\n ${res.data.result.lirik}`
 	conn.sendMessage(id, hasil, MessageType.text)
 	})
 }
